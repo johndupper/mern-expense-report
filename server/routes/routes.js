4311dbcf1800f0b60898ac2b0c69014e1982c90e
John Dupper <dupper.john@gmail.com>
2018-10-15 12:49:20 -0400

    created nodejs server crud for expenses

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,72 @@
+// https://blog.cloudboost.io/creating-your-first-mern-stack-application-b6604d12e4d3
 const express = require('express')
+const Expense = require('../../models/Expense')
+
 const router = express.Router()
+
+const parseExpense = req => {
+  const { id, description, amount, month, year } = req.body
+  const expense = { description, amount, month, year }
+  if (id) expense.id = id
+  return expense
+}
 
 router.get('/', (req, res) => {
   res.render('index')
+})
+
+// create
+router.post('/insert', (req, res) => {
+  const expense = new Expense(parseExpense(req))
+
+  expense.save((error, expense) => {
+    return error
+      ? res.status(500).json({ error })
+      : res.status(201).json({ expense })
+  })
+})
+
+// get all
+router.get('/getAll', (req, res) => {
+  Expense.find({}, (error, expenses) => {
+    return error
+      ? res.status(500).json({ error })
+      : res.status(200).json({ expenses })
+  })
+})
+
+// get by id
+router.get('/get/:id', (req, res) => {
+  const id = req.params.id
+
+  Expense.findById(id, (error, expense) => {
+    return error
+      ? res.status(500).json({ error })
+      : res.status(200).json({ expense })
+  })
+})
+
+// update
+router.put('/update', (req, res) => {
+  const expense = parseExpense(req)
+  const id = expense.id
+
+  Expense.update({ id }, expense, (error, expense) => {
+    return error
+      ? res.status(500).json({ error })
+      : res.status(200).json({ expense })
+  })
+})
+
+// delete
+router.delete('/delete/:id', (req, res) => {
+  const id = req.params.id
+
+  Expense.findByIdAndDelete(id, (error, expense) => {
+    return error
+      ? res.status(500).json({ error })
+      : res.status(200).json({ expense })
+  })
 })
 
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
+require('dotenv').config()
+const username = process.env.MONGO_USER
+const password = process.env.MONGO_PASSWORD
+const dbUrl = `mongodb://${username}:${password}@ds131973.mlab.com:31973/expenses`
+
 const express = require('express')
-const router = require('./routes/routes.js')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 const path = require('path')
+const router = require('./routes/routes.js')
+
 const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '../client'))
 
 app.use(express.static(path.join(__dirname, '../client')))
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }))
+
 app.use('/', router)
+
+mongoose.connect(dbUrl, { useNewUrlParser: true })
+  .then(() => { console.log('connected to mongoDB successfully') })
+  .catch(e => { console.error(`could not connect to mongoDB: ${e.message}`) })
 
 module.exports = app
